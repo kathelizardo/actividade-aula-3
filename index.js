2c763e0d41d6af886faec7461406b00939aa8803
Kathe Lizardo <lizardokm@gmail.com>
2021-04-23 14:20:26 -0300

    erro de ortografia corrigido

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ let nome=prompt('Digite seu nome')
 let sobrenome= prompt('Digite seu sobrenome') 
 let idade= Number(prompt('Digite sua idade'))
 let nacionalidade=prompt( 'Nacionalidade') 
-let ciudade= prompt('Ciudade de Residencia')
+let ciudade= prompt('Ciudade de Residência')
 
 console.log('Nome:' + nome + (' ')+ sobrenome )
-console.log('Idade:' + idade)
+console.log('Idade:' + idade + (' ') + 'anos')
 console.log('Nacionalidade:' + nacionalidade)
 console.log('Recidencia:' + ciudade)
-console.log ('seu registro foi bem sucedido')
+console.log ('Seu registro foi bem sucedido')
 console.log ('Bem-vindo')
 
 var dados = {
